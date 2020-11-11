343708cad52ca24e174a108eacfede9990f76e76
Nicolas Triquenaux <nicolas.triquenaux@dataiku.com>
2020-11-11 16:04:35 +0100

    adding the user management for tests [ch55890]

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -1,25 +1,32 @@
-from dku_plugin_test_utils import scenario
+import pytest
+import logging
+
+from dku_plugin_test_utils import dss_scenario
 
 
-def test_run_read_zendesk_groups(client, plugin):
-    scenario(client, 'PLUGINTESTZENDESK', 'run_read_zendesk_groups')
+pytestmark = pytest.mark.usefixtures("plugin", "dss_target")
+logger = logging.getLogger("dss-plugin-test.zendesk.test_scenario")
 
 
-def test_run_read_zendesk_incremental(client, plugin):
-    scenario(client, 'PLUGINTESTZENDESK', 'run_read_zendesk_incremental')
+def test_run_read_zendesk_groups(user_clients):
+    dss_scenario.run("default", user_clients["default"], 'PLUGINTESTZENDESK', 'run_read_zendesk_groups', logger)
 
 
-def test_run_read_zendesk_organizations(client, plugin):
-    scenario(client, 'PLUGINTESTZENDESK', 'run_read_zendesk_organizations')
+def test_run_read_zendesk_incremental(user_clients):
+    dss_scenario.run("default", user_clients["default"], 'PLUGINTESTZENDESK', 'run_read_zendesk_incremental', logger)
 
 
-def test_run_read_zendesk_search(client, plugin):
-    scenario(client, 'PLUGINTESTZENDESK', 'run_read_zendesk_search')
+def test_run_read_zendesk_organizations(user_clients):
+    dss_scenario.run("user1", user_clients["user1"], 'PLUGINTESTZENDESK', 'run_read_zendesk_organizations', logger)
 
 
-def test_run_read_zendesk_tickets(client, plugin):
-    scenario(client, 'PLUGINTESTZENDESK', 'run_read_zendesk_tickets')
+def test_run_read_zendesk_search(user_clients):
+    dss_scenario.run("user1", user_clients["user1"], 'PLUGINTESTZENDESK', 'run_read_zendesk_search', logger)
 
 
-def test_run_read_zendesk_users(client, plugin):
-    scenario(client, 'PLUGINTESTZENDESK', 'run_read_zendesk_users')
+def test_run_read_zendesk_tickets(user_clients):
+    dss_scenario.run("user1", user_clients["user1"], 'PLUGINTESTZENDESK', 'run_read_zendesk_tickets', logger)
+
+
+def test_run_read_zendesk_user_clients(user_clients):
+    dss_scenario.run("user1", user_clients["user1"], 'PLUGINTESTZENDESK', 'run_read_zendesk_users', logger)
