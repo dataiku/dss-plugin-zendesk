e4487eb50c7873ec5ed81a4b7d07e637100b8603
HenriChabert <henri@famille-chabert.com>
2021-04-06 12:05:51 +0200

    Fixed user name with new dku34

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -14,12 +14,12 @@ def test_run_read_zendesk_organizations(user_dss_clients):
 
 
 def test_run_read_zendesk_search(user_dss_clients):
-    dss_scenario.run(user_dss_clients, 'PLUGINTESTZENDESK', 'run_read_zendesk_search', user="user1")
+    dss_scenario.run(user_dss_clients, 'PLUGINTESTZENDESK', 'run_read_zendesk_search', user="data_scientist_1")
 
 
 def test_run_read_zendesk_tickets(user_dss_clients):
-    dss_scenario.run(user_dss_clients, 'PLUGINTESTZENDESK', 'run_read_zendesk_tickets', user="user1")
+    dss_scenario.run(user_dss_clients, 'PLUGINTESTZENDESK', 'run_read_zendesk_tickets', user="data_scientist_1")
 
 
 def test_run_read_zendesk_user_clients(user_dss_clients):
-    dss_scenario.run(user_dss_clients, 'PLUGINTESTZENDESK', 'run_read_zendesk_users', user="user1")
+    dss_scenario.run(user_dss_clients, 'PLUGINTESTZENDESK', 'run_read_zendesk_users', user="data_scientist_1")
